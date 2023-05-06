61f545f6ac73c48591f320d541def688b94bb88b
Mateus Macedo <mateusouza2014@live.com>
2023-05-06 19:43:27 -0300

    feat: Adicionando estrutura de pastas clean arq

---
+++ MS-SolicitaAprovacaoParcela/inss-libs/src/main/kotlin/br/com/solicita/aprovacao/parcela/domain/entities/AprovacaoEntity.kt
@@ -0,0 +1,46 @@
+package br.com.solicita.aprovacao.parcela.domain.entities
+
+import br.com.solicita.aprovacao.parcela.domain.Entity
+import br.com.solicita.aprovacao.parcela.domain.exceptions.InvalidCepFormatException
+import br.com.solicita.aprovacao.parcela.domain.exceptions.RequiredFieldException
+import java.util.*
+
+data class NewAddress(
+        val cep: String,
+        val street: String,
+        val district: String,
+        val city: String,
+        val state: String
+) {
+    fun create() = AddressEntity(UUID.randomUUID(), cep, street, district, city, state)
+}
+
+
+data class AddressEntity(
+        override val uuid: UUID,
+        val cep: String,
+        val street: String,
+        val district: String,
+        val city: String,
+        val state: String
+) : Entity {
+    init {
+        if (cep == null || cep == "")
+            throw RequiredFieldException("cep")
+
+        if (!cep.matches(Regex("[0-9]{8}")))
+            throw InvalidCepFormatException()
+
+        if (street == null || street == "")
+            throw RequiredFieldException("street")
+
+        if (district == null || district == "")
+            throw RequiredFieldException("district")
+
+        if (city == null || city == "")
+            throw RequiredFieldException("city")
+
+        if (state == null || state == "")
+            throw RequiredFieldException("state")
+    }
+}
---
+++ MS-SolicitaAprovacaoParcela/inss-libs/src/main/kotlin/br/com/solicita/aprovacao/parcela/domain/exceptions/DomainExceptions.kt
@@ -0,0 +1,4 @@
+package br.com.solicita.aprovacao.parcela.domain.exceptions
+
+class DomainExceptions {
+}
